9c03b24f9247d753a77f64e02e1811b4e3c2dc01
Erik Gonzalez <erigonza@car7s3.42barcelona.com>
2024-01-24 14:05:00 +0100

    My first push. Proyect almost finished. Need to fix Makefile and stop being a dick

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erigonza <erigonza@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 19:22:56 by erigonza          #+#    #+#             */
+/*   Updated: 2024/01/20 13:13:01 by erigonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*str;
+
+	str = malloc(count * size);
+	if (str == 0)
+		return (0);
+	ft_bzero(str, count * size);
+	return (str);
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erigonza <erigonza@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 16:09:06 by erigonza          #+#    #+#             */
+/*   Updated: 2024/01/13 22:24:07 by erigonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t		i;
+	size_t		j;
+	char		*str;
+
+	i = 0;
+	j = 0;
+	if (!(s1 && s2))
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char *a = "";
+	char *b = "42";
+	printf("%s\n", ft_strjoin(a, b));
+}*/
